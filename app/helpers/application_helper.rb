15506a500ca9db91ded11c576cf8b47947a74622
rahul-das <rahul.das@bitcanny.com>
2019-02-14 19:28:14 +0530

    Days not needed

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,23 +4,13 @@ module ApplicationHelper
   end
 
   def seconds_to_dhm(seconds)
-    days = (seconds / (24 * 60 * 60)).floor
-    seconds = seconds % (24 * 60 * 60)
     hours = (seconds / (60 * 60)).floor
     seconds = seconds % (60 * 60)
     minutes = (seconds / 60.0).ceil
-    if days == 0
-      if hours == 0
-        return "#{minutes}m"
-      else
-        return "#{hours}h #{minutes}m"
-      end
+    if hours == 0
+      return "#{minutes}m"
     else
-      if hours == 0
-        return "#{days}d #{minutes}m"
-      else
-        return "#{days}d #{hours}h #{minutes}m"
-      end
+      return "#{hours}h #{minutes}m"
     end
   end
 end
