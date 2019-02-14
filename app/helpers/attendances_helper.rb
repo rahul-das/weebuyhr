74ee4ebed07617adedba83d51995b9fab4107825
rahul-das <rahul.das@bitcanny.com>
2019-02-14 17:42:28 +0530

    Attendance Duration added

--- app/helpers/attendances_helper.rb
+++ app/helpers/attendances_helper.rb
@@ -1,2 +1,11 @@
 module AttendancesHelper
+  def time_diff(from_time, to_time)
+    hours = ((to_time - from_time).round / 3600)
+    minutes = (((to_time - from_time).round % 3600.0) / 60.0).ceil
+    if hours == 0
+      return "#{minutes} minute(s)"
+    else
+      return "#{hours} hour(s) #{minutes} minute(s)"
+    end
+  end
 end
